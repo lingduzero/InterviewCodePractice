24b68edc60e3c9eae0167f6607f8ec39678ba79f
lingduzero <bz63@njit.edu>
2020-03-20 23:14:29 -0700

    fix the solution

--- 88.merge-sorted-array.js
+++ 88.merge-sorted-array.js
@@ -13,23 +13,15 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let i = m - 1;
-    let j = n - 1;
+    let index1 = m - 1;
+    let index2 = n - 1;
     let index = m + n - 1;
-    while(i >= 0 && j >= 0) {
-        if(nums1[i] < nums2[j]) {
-            nums1[index--] = nums2[j--];
+    while(index2 >= 0) {
+        if(nums1[index1] > nums2[index2]) {
+            nums1[index--] = nums1[index1--];
         } else {
-            nums1[index--] = nums1[i--];
+            nums1[index--] = nums2[index2--];
         }
-    }
-
-    while(i >= 0) {
-        nums1[index--] = nums1[i--];
-    }
-
-    while(j >= 0) {
-        nums1[index--] = nums2[j--];
     }
 };
 // @lc code=end
